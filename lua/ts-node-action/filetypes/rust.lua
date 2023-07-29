1153bc04ac1dabce455cfc610218bd6bd7f4f574
cbows <32486983+cbows@users.noreply.github.com>
2023-07-29 17:42:12 +0200

    style: apply formatting

--- lua/ts-node-action/filetypes/rust.lua
+++ lua/ts-node-action/filetypes/rust.lua
@@ -13,8 +13,8 @@ local operators = {
   -- comparison
   ["!="] = "==",
   ["=="] = "!=",
-  [">"]  = "<",
-  ["<"]  = ">",
+  [">"] = "<",
+  ["<"] = ">",
   [">="] = "<=",
   ["<="] = ">=",
   -- shift
@@ -37,8 +37,8 @@ local operators = {
 }
 
 local padding = {
-  ["{"]   = "%s ",
-  ["}"]   = {
+  ["{"] = "%s ",
+  ["}"] = {
     " %s",
     ["{"] = "%s",
     [","] = "%s",
@@ -46,45 +46,45 @@ local padding = {
     ["prev_nil"] = "%s",
   },
   ["let"] = "%s ",
-  ["as"]  = " %s ",
-  ["in"]  = { " %s ", ["prev_nil"] = "%s ", },
-  ["="]   = " %s ",
-  [":"]   = "%s ",
-  [";"]   = "%s ",
-  [","]   = "%s ",
+  ["as"] = " %s ",
+  ["in"] = { " %s ", ["prev_nil"] = "%s " },
+  ["="] = " %s ",
+  [":"] = "%s ",
+  [";"] = "%s ",
+  [","] = "%s ",
 }
 local padding_use_list = {
-  ["{"]  = "%s",
-  ["}"]  = "%s",
-  [","]  = "%s ",
+  ["{"] = "%s",
+  ["}"] = "%s",
+  [","] = "%s ",
 }
 
 local uncollapsible = {
-  ["string_literal"]       = true,
-  ["let_declaration"]      = true,
-  ["reference_type"]       = true,
+  ["string_literal"] = true,
+  ["let_declaration"] = true,
+  ["reference_type"] = true,
   ["reference_expression"] = true,
   ["type_case_expression"] = true,
-  ["macro_invocation"]     = true,
-  ["macro"]                = true,
-  ["for_expression"]       = true,
-  ["range_expression"]     = true,
-  ["line_comment"]         = true,
+  ["macro_invocation"] = true,
+  ["macro"] = true,
+  ["for_expression"] = true,
+  ["range_expression"] = true,
+  ["line_comment"] = true,
 }
 
 return {
-  ["boolean_literal"]          = actions.toggle_boolean(),
-  ["integer_literal"]          = actions.toggle_int_readability(),
-  ["binary_expression"]        = actions.toggle_operator(operators),
+  ["boolean_literal"] = actions.toggle_boolean(),
+  ["integer_literal"] = actions.toggle_int_readability(),
+  ["binary_expression"] = actions.toggle_operator(operators),
   ["compound_assignment_expr"] = actions.toggle_operator(operators),
-  ["use_list"]                 = actions.toggle_multiline(padding_use_list, uncollapsible),
-  ["block"]                    = actions.toggle_multiline(padding, uncollapsible),
-  ["parameters"]               = actions.toggle_multiline(padding, uncollapsible),
-  ["arguments"]                = actions.toggle_multiline(padding, uncollapsible),
-  ["array_expression"]         = actions.toggle_multiline(padding, uncollapsible),
-  ["tuple_expression"]         = actions.toggle_multiline(padding, uncollapsible),
-  ["tuple_pattern"]            = actions.toggle_multiline(padding, uncollapsible),
-  ["enum_variant_list"]        = actions.toggle_multiline(padding, uncollapsible),
-  ["field_initializer_list"]   = actions.toggle_multiline(padding, uncollapsible),
-  ["field_declaration_list"]   = actions.toggle_multiline(padding, uncollapsible),
+  ["use_list"] = actions.toggle_multiline(padding_use_list, uncollapsible),
+  ["block"] = actions.toggle_multiline(padding, uncollapsible),
+  ["parameters"] = actions.toggle_multiline(padding, uncollapsible),
+  ["arguments"] = actions.toggle_multiline(padding, uncollapsible),
+  ["array_expression"] = actions.toggle_multiline(padding, uncollapsible),
+  ["tuple_expression"] = actions.toggle_multiline(padding, uncollapsible),
+  ["tuple_pattern"] = actions.toggle_multiline(padding, uncollapsible),
+  ["enum_variant_list"] = actions.toggle_multiline(padding, uncollapsible),
+  ["field_initializer_list"] = actions.toggle_multiline(padding, uncollapsible),
+  ["field_declaration_list"] = actions.toggle_multiline(padding, uncollapsible),
 }
